c3bd807ad78972c8fa9d8d01fad0966ff9c651ff
Carolyn Caron <carolyn.caron@usask.ca>
2023-04-19 17:21:46 -0600

    Initialized test for plugin base getter and setter methods

---
+++ trpcultivate_genotypes/tests/src/Functional/GenotypesLoaderTest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Drupal\Tests\trpcultivate_genotypes\Functional;
+
+use Drupal\Core\Url;
+use Drupal\Tests\tripal_chado\Functional\ChadoTestBrowserBase;
+
+/**
+ * A test to call the setter and getter methods in the plugin base for the genotypes loader.
+ *
+ * @group TripGeno Genetics
+ * @group Genotypes Loader
+ */
+class GetterSetterTest extends ChadoTestBrowserBase {
+
+    // Create the Genotypes Loader object
+    // $plugin_manager = \Drupal::service('plugin.manager.genotypes_loader');
+
+
+
+
+}
