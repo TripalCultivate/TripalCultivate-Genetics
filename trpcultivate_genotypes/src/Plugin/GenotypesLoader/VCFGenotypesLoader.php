086c3037ffb7b28a0d0d706ae7688e8e70e6f9f7
Vidusha Wilpita <vwilpita@yahoo.com>
2026-03-05 17:59:08 +0000

    resolved the Not supporting attribute discovery in Drupal\trpcultivate_genotypes\GenotypesLoader\GenotypesLoaderPluginManager is deprecated deprecation using a new attribute class for GenotypesLoader

--- trpcultivate_genotypes/src/Plugin/GenotypesLoader/VCFGenotypesLoader.php
+++ trpcultivate_genotypes/src/Plugin/GenotypesLoader/VCFGenotypesLoader.php
@@ -2,23 +2,24 @@
 
 namespace Drupal\trpcultivate_genotypes\Plugin\GenotypesLoader;
 
+use Drupal\Core\StringTranslation\TranslatableMarkup;
+use Drupal\trpcultivate_genotypes\Attribute\GenotypesLoader;
 use Drupal\trpcultivate_genotypes\GenotypesLoader\GenotypesLoaderPluginBase;
 use Drupal\trpcultivate_genotypes\GenotypesLoader\GenotypesLoaderInterface;
 
 /**
  * Provides a plugin for loading genotypes from a VCF file.
- *
- *  @GenotypesLoader(
- *    id = "vcf_genotypes_loader",
- *    label = @Translation("VCF Genotypes Loader"),
- *    description = @Translation("Handles the loading of genotypes from a VCF file."),
- *    input_file_type = "vcf"
- *  )
  */
+#[GenotypesLoader(
+  id: "vcf_genotypes_loader",
+  title: new TranslatableMarkup('VCF Genotypes Loader'),
+  description: new TranslatableMarkup('Handles the loading of genotypes from a VCF file.'),
+  input_file_type: "vcf"
+)]
 class VCFGenotypesLoader extends GenotypesLoaderPluginBase implements GenotypesLoaderInterface {
-  
+
   /**
-   * 
+   *
    */
 
 }
